449db65c7ef09c92cf6d23012af0c78e54328ebc
Denis Safonov <denissafonov_85@mail.ru>
2025-05-18 19:03:58 +0500

    Добавлен модуль processing.py с функцией filter_by_sate

---
+++ src/processing.py
@@ -0,0 +1,11 @@
+def filter_by_state(list_of_dic: list, state="EXECUTED") -> list:
+    """
+    Функция принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED')
+    Возвращает новый список словарей, содержащий только те словари,
+    у которых ключ state соответствует указанному значению.
+    """
+    new_list_of_dic = []
+    for dic in list_of_dic:
+        if dic.get("state") == state:
+            new_list_of_dic.append(dic)
+    return new_list_of_dic
