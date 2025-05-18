ce4ef2b1472d793cbe1a0ca0ade31b830b9b8d64
Denis Safonov <denissafonov_85@mail.ru>
2025-05-18 17:59:34 +0500

    fixed main.py

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 from src.masks import get_mask_account, get_mask_card_number
 from src.widget import get_date, mask_account_card
 
-# Ввод данных о номере карты и счета
-card_number = int(input("Введите номер карты: "))
-account_number = int(input("Введите номер счета: "))
+if __name__ == "__main__":
+    # Ввод данных о номере карты и счета
+    card_number = int(input("Введите номер карты: "))
+    account_number = int(input("Введите номер счета: "))
+    user_card_or_account_number = input("Введите номер карты или счета: ")
 
-# Вывод работы функций
-print(f"Номер карты: {get_mask_card_number(card_number)}")
-print(f"Номер счета: {get_mask_account(account_number)}")
-print("Примеры входных данных для проверки функции mask_account_card")
-print(mask_account_card("Maestro 1596837868705199"))
-print(mask_account_card("Счет 64686473678894779589"))
-print(mask_account_card("MasterCard 7158300734726758"))
-print(mask_account_card("Счет 35383033474447895560"))
-print(mask_account_card("Visa Classic 6831982476737658"))
-print(mask_account_card("Visa Platinum 8990922113665229"))
-print(mask_account_card("Visa Gold 5999414228426353"))
-print(mask_account_card("Счет 73654108430135874305"))
-print(get_date("2024-03-11T02:26:18.671407"))
+    # Вывод работы функций
+    print(f"Номер карты: {get_mask_card_number(card_number)}")
+    print(f"Номер счета: {get_mask_account(account_number)}")
+    print(mask_account_card(user_card_or_account_number))
+    print(get_date("2024-03-11T02:26:18.671407"))
