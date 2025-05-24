76a413f42f04874c3ac5aef46aa35fee6be88694
Denis Safonov <denissafonov_85@mail.ru>
2025-05-24 16:19:56 +0500

    Добавлена параметризация для тестируемых функций

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -3,12 +3,58 @@ import pytest
 from src.generator import card_number_generator, filter_by_currency, transaction_descriptions
 
 
+@pytest.mark.parametrize("value, code, expected", [
+        (
+            [
+                {
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                    "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+                    "description": "Перевод организации",
+                    "from": "Счет 75106830613657916952",
+                    "to": "Счет 11776614605963066702",
+                }
+            ],
+            "USD",
+            {
+                "id": 939719570,
+                "state": "EXECUTED",
+                "date": "2018-06-30T02:08:58.425572",
+                "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+                "description": "Перевод организации",
+                "from": "Счет 75106830613657916952",
+                "to": "Счет 11776614605963066702",
+            }
+        ),
+        (
+            [
+                {
+                    "id": 873106923,
+                    "state": "EXECUTED",
+                    "date": "2019-03-23T01:09:46.296404",
+                    "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+                    "description": "Перевод со счета на счет",
+                    "from": "Счет 44812258784861134719",
+                    "to": "Счет 74489636417521191160"
+                }
+            ],
+            "RUB",
+            {
+                "id": 873106923,
+                "state": "EXECUTED",
+                "date": "2019-03-23T01:09:46.296404",
+                "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+                "description": "Перевод со счета на счет",
+                "from": "Счет 44812258784861134719",
+                "to": "Счет 74489636417521191160"
+            }
+        )
+    ]
+)
 # Тест test_filter_by_currency на корректные данные
-def test_filter_by_currency(data, filter_by_currency_usd_1, filter_by_currency_usd_2, filter_by_currency_usd_3):
-    iterator = filter_by_currency(data, "USD")
-    assert next(iterator) == filter_by_currency_usd_1
-    assert next(iterator) == filter_by_currency_usd_2
-    assert next(iterator) == filter_by_currency_usd_3
+def test_filter_by_currency(value, code, expected):
+    assert next(filter_by_currency(value, code)) == expected
 
 
 # Тест test_filter_by_currency на отсутствие ключа
@@ -28,12 +74,42 @@ def test_filter_by_currency_zero():
 
 
 # Тест test_transaction_descriptions на корректные данные
-def test_transaction_descriptions(data):
-    trans_descrip = transaction_descriptions(data)
-    assert next(trans_descrip) == "Перевод организации"
-    assert next(trans_descrip) == "Перевод со счета на счет"
-    assert next(trans_descrip) == "Перевод со счета на счет"
-    assert next(trans_descrip) == "Перевод с карты на карту"
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        (
+            [
+                {
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                    "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+                    "description": "Перевод организации",
+                    "from": "Счет 75106830613657916952",
+                    "to": "Счет 11776614605963066702",
+                },
+            ],
+            "Перевод организации",
+        ),
+        (
+            [
+                {
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                    "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+                    "description": "Перевод со счета на счет",
+                    "from": "Счет 75106830613657916952",
+                    "to": "Счет 11776614605963066702",
+                },
+            ],
+            "Перевод со счета на счет",
+        )
+    ],
+)
+def test_transaction_descriptions(value, expected):
+    trans_descrip = transaction_descriptions(value)
+    assert next(trans_descrip) == expected
 
 
 # Тест test_transaction_descriptions на пустой список
@@ -45,14 +121,14 @@ def test_transaction_descriptions_zero():
 
 
 # Тест test_card_number_generator на корректные данные
-def test_card_number_generator():
-    trans_descrip = card_number_generator(10, 15)
-    assert next(trans_descrip) == "0000 0000 0000 0010"
-    assert next(trans_descrip) == "0000 0000 0000 0011"
-    assert next(trans_descrip) == "0000 0000 0000 0012"
-    assert next(trans_descrip) == "0000 0000 0000 0013"
-    assert next(trans_descrip) == "0000 0000 0000 0014"
-    assert next(trans_descrip) == "0000 0000 0000 0015"
+@pytest.mark.parametrize("start, stop, expected", [
+    (10, 15, "0000 0000 0000 0010"),
+    (99995555, 99995558, "0000 0000 9999 5555"),
+    (1, 1, "0000 0000 0000 0001")
+])
+def test_card_number_generator(start, stop, expected):
+    new_card = card_number_generator(start, stop)
+    assert next(new_card) == expected
 
 
 # Тест test_card_number_generator на некорректные данные
