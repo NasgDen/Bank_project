aed42e241d1041d56564001964c03f10598729a7
Denis Safonov <denissafonov_85@mail.ru>
2025-06-11 23:51:20 +0500

    В модуль processing.py добавлена функция process_bank_search

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,5 @@
+import re
+
 from src.decorators import log
 
 
@@ -17,3 +19,16 @@ def sort_by_date(list_of_dic: list, descending=True) -> list:
     Функция возвращать новый список, отсортированный по дате.
     """
     return sorted(list_of_dic, key=lambda date: date.get("date"), reverse=descending)
+
+
+def process_bank_search(data: list[dict], search: str) -> list[dict]:
+    """
+    Функцию принимать список словарей с данными о банковских операциях и строку поиска.
+    Возвращает список словарей, у которых в описании есть данная строка.
+    """
+    data_find = []
+    for data_search in data:
+        match = re.search(search, str(data_search["description"]), re.IGNORECASE)
+        if match:
+            data_find.append(data_search)
+    return data_find
